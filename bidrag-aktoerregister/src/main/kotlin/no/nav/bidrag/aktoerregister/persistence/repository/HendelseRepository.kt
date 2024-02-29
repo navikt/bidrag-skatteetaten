cdc17486cbc1dea6a768804b03ed23ba9ea0d979
Simen Høston <simen.naper.hoston@webstep.no>
2024-02-29 20:30:48 +0100

    Endrer uthenting av hendelser til å kun hente kolonneverdier fra hendelsen slik at man slipper å ha en referanse til aktøren for alle hendelsen

--- bidrag-aktoerregister/src/main/kotlin/no/nav/bidrag/aktoerregister/persistence/repository/HendelseRepository.kt
+++ bidrag-aktoerregister/src/main/kotlin/no/nav/bidrag/aktoerregister/persistence/repository/HendelseRepository.kt
@@ -3,8 +3,22 @@ package no.nav.bidrag.aktoerregister.persistence.repository
 import no.nav.bidrag.aktoerregister.persistence.entities.Hendelse
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface HendelseRepository : JpaRepository<Hendelse, Int> {
 
-    fun findBySekvensnummerGreaterThan(sekvensnummer: Int, pageable: Pageable): List<Hendelse>
+    @Query(
+        value = """
+            SELECT sekvensnummer, aktoer_ident
+            FROM  aktoerregister.hendelse h
+            WHERE h.sekvensnummer >= :sekvensnummer
+        """,
+        nativeQuery = true,
+    )
+    fun hentAlleHendelserMedSekvensnummerOgIdent(sekvensnummer: Int, pageable: Pageable): List<SekvensnummerOgIdent>
+}
+
+interface SekvensnummerOgIdent {
+    val sekvensnummer: Int
+    val aktoerIdent: String
 }
