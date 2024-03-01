c8391b996c0919623c8dfbee85833c81367e4cd3
Simen Høston <simen.naper.hoston@webstep.no>
2024-03-01 08:39:48 +0100

    Endrer navn på inferface objektet i query til å matche datbasenavnet, endrer fra pageable til limit da vi ikke bruker pageables funksjonalitet annet en som en limit

--- bidrag-aktoerregister/src/main/kotlin/no/nav/bidrag/aktoerregister/persistence/repository/HendelseRepository.kt
+++ bidrag-aktoerregister/src/main/kotlin/no/nav/bidrag/aktoerregister/persistence/repository/HendelseRepository.kt
@@ -1,7 +1,7 @@
 package no.nav.bidrag.aktoerregister.persistence.repository
 
 import no.nav.bidrag.aktoerregister.persistence.entities.Hendelse
-import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Limit
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 
@@ -12,13 +12,14 @@ interface HendelseRepository : JpaRepository<Hendelse, Int> {
             SELECT sekvensnummer, aktoer_ident
             FROM  aktoerregister.hendelse h
             WHERE h.sekvensnummer >= :sekvensnummer
+            ORDER BY sekvensnummer
         """,
         nativeQuery = true,
     )
-    fun hentAlleHendelserMedSekvensnummerOgIdent(sekvensnummer: Int, pageable: Pageable): List<SekvensnummerOgIdent>
+    fun hentAlleHendelserMedSekvensnummerOgIdent(sekvensnummer: Int, limit: Limit): List<SekvensnummerOgIdent>
 }
 
 interface SekvensnummerOgIdent {
     val sekvensnummer: Int
-    val aktoerIdent: String
+    val aktoer_ident: String
 }
