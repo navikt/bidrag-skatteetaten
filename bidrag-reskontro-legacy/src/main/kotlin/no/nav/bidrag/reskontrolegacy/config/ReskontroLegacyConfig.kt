1678532e8d605927570e889c911d73705ff30368
Simen Høston <simen.naper.hoston@webstep.no>
2024-03-07 13:01:59 +0100

    Prøver å fjerne bisys dependency for å se om det er den som ødelegger swagger

--- bidrag-reskontro-legacy/src/main/kotlin/no/nav/bidrag/reskontrolegacy/config/ReskontroLegacyConfig.kt
+++ bidrag-reskontro-legacy/src/main/kotlin/no/nav/bidrag/reskontrolegacy/config/ReskontroLegacyConfig.kt
@@ -8,9 +8,6 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme
 import no.nav.bidrag.commons.web.DefaultCorsFilter
 import no.nav.bidrag.commons.web.MdcFilter
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
-import no.spn.www.BisysReskWSSoapProxy
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.EnableAspectJAutoProxy
 import org.springframework.context.annotation.Import
@@ -28,10 +25,4 @@ import org.springframework.context.annotation.Import
 @SecurityScheme(bearerFormat = "JWT", name = "bearer-key", scheme = "bearer", type = SecuritySchemeType.HTTP)
 @EnableJwtTokenValidation
 @Import(DefaultCorsFilter::class, MdcFilter::class)
-class ReskontroLegacyConfig {
-
-    @Bean
-    fun reskWsSoapProxy(@Value("\${ELIN_URL}") elinUrl: String): BisysReskWSSoapProxy {
-        return BisysReskWSSoapProxy(elinUrl)
-    }
-}
+class ReskontroLegacyConfig
