e4d352bb2c3cec38516b1778ab83e52a67ee98f9
Simen Høston <simen.naper.hoston@webstep.no>
2024-03-08 11:07:05 +0100

    Endrer til å bruke limit i native query i steden for via spring data Limit.of

--- bidrag-aktoerregister/src/main/kotlin/no/nav/bidrag/aktoerregister/persistence/repository/HendelseRepository.kt
+++ bidrag-aktoerregister/src/main/kotlin/no/nav/bidrag/aktoerregister/persistence/repository/HendelseRepository.kt
@@ -1,7 +1,6 @@
 package no.nav.bidrag.aktoerregister.persistence.repository
 
 import no.nav.bidrag.aktoerregister.persistence.entities.Hendelse
-import org.springframework.data.domain.Limit
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 
@@ -13,10 +12,11 @@ interface HendelseRepository : JpaRepository<Hendelse, Int> {
             FROM  aktoerregister.hendelse h
             WHERE h.sekvensnummer >= :sekvensnummer
             ORDER BY sekvensnummer
+            LIMIT :limit
         """,
         nativeQuery = true,
     )
-    fun hentAlleHendelserMedSekvensnummerOgIdent(sekvensnummer: Int, limit: Limit): List<SekvensnummerOgIdent>
+    fun hentAlleHendelserMedSekvensnummerOgIdent(sekvensnummer: Int, limit: Int): List<SekvensnummerOgIdent>
 }
 
 interface SekvensnummerOgIdent {
