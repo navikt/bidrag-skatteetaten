e62f31d314e3ed8dd809607af3dced2f1eb4a868
Simen Høston <simen.naper.hoston@webstep.no>
2025-04-01 09:57:00 +0200

    Fjerner debug-endepunkt for ip adresse og ssl serfikat

--- bidrag-regnskap/src/main/kotlin/no/nav/bidrag/regnskap/controller/PatchController.kt
+++ bidrag-regnskap/src/main/kotlin/no/nav/bidrag/regnskap/controller/PatchController.kt
@@ -6,11 +6,6 @@ import no.nav.security.token.support.core.api.Protected
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
-import java.io.IOException
-import java.net.URI
-import java.net.http.HttpClient
-import java.net.http.HttpRequest
-import java.net.http.HttpResponse
 
 @Protected
 @RestController
@@ -19,18 +14,5 @@ class PatchController(private val oppdragService: OppdragService) {
     @PostMapping("/patchMottaker")
     fun patchMottaker(@RequestBody patchMottakerRequest: PatchMottakerRequest) {
         oppdragService.patchMottaker(patchMottakerRequest.saksnummer, patchMottakerRequest.kravhaver, patchMottakerRequest.mottaker)
-    }
-
-    @PostMapping("/ip")
-    fun hentIp(): String = try {
-        val client = HttpClient.newHttpClient()
-        val request = HttpRequest.newBuilder()
-            .uri(URI.create("https://api.ipify.org"))
-            .build()
-        val response = client.send(request, HttpResponse.BodyHandlers.ofString())
-        "My current IP address is ${response.body()}"
-    } catch (e: IOException) {
-        e.printStackTrace()
-        "Unable to fetch IP address"
     }
 }
