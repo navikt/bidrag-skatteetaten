79de48da60b0da8b269365a85530500e9cc42ac3
Simen Høston <simen.naper.hoston@webstep.no>
2025-06-17 13:09:14 +0200

    Oppretter kobling mot bidrag-vedtak

---
+++ bidrag-regnskap/src/main/kotlin/no/nav/bidrag/regnskap/consumer/BidragVedtakConsumer.kt
@@ -0,0 +1,27 @@
+package no.nav.bidrag.regnskap.consumer
+
+import no.nav.bidrag.commons.web.client.AbstractRestClient
+import no.nav.bidrag.transport.behandling.vedtak.response.VedtakDto
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.util.UriComponentsBuilder
+import java.net.URI
+
+@Service
+class BidragVedtakConsumer(
+    @Value("\${BIDRAG_VEDTAK_URL}") private val bidragVedtakUrl: URI,
+    @Qualifier("azure") restTemplate: RestTemplate,
+) : AbstractRestClient(restTemplate, "bidrag-vedtak") {
+    private val bidragVedtakUri
+        get() = UriComponentsBuilder.fromUri(bidragVedtakUrl)
+
+    fun hentVedtak(vedtakId: Int): VedtakDto? = getForEntity(
+        bidragVedtakUri
+            .pathSegment("vedtak")
+            .pathSegment(vedtakId.toString())
+            .build()
+            .toUri(),
+    )
+}
