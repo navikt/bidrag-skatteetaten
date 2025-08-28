95ceb6c2ea1a7d4acf77fbdcb15ba5a7b2b433ef
Simen Høston <simen.naper.hoston@webstep.no>
2025-08-28 14:17:13 +0200

    Kommenterer ut ubrukt cache

--- bidrag-reskontro/src/main/kotlin/no/nav/bidrag/reskontro/config/CacheConfig.kt
+++ bidrag-reskontro/src/main/kotlin/no/nav/bidrag/reskontro/config/CacheConfig.kt
@@ -1,12 +1,7 @@
 package no.nav.bidrag.reskontro.config
 
-import com.github.benmanes.caffeine.cache.Caffeine
 import no.nav.bidrag.commons.cache.EnableUserCache
-import no.nav.bidrag.commons.cache.InvaliderCacheFørStartenAvArbeidsdag
-import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.EnableCaching
-import org.springframework.cache.caffeine.CaffeineCacheManager
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 
@@ -15,17 +10,17 @@ import org.springframework.context.annotation.Profile
 @Profile(value = ["!test"]) // Ignore cache on tests
 @EnableUserCache
 class CacheConfig {
-    companion object {
-        const val PERSON_CACHE = "PERSON_CACHE"
-    }
-
-    @Bean
-    fun cacheManager(): CacheManager {
-        val caffeineCacheManager = CaffeineCacheManager()
-        caffeineCacheManager.registerCustomCache(
-            PERSON_CACHE,
-            Caffeine.newBuilder().expireAfter(InvaliderCacheFørStartenAvArbeidsdag()).build(),
-        )
-        return caffeineCacheManager
-    }
+//    companion object {
+//        const val PERSON_CACHE = "PERSON_CACHE"
+//    }
+//
+//    @Bean
+//    fun cacheManager(): CacheManager {
+//        val caffeineCacheManager = CaffeineCacheManager()
+//        caffeineCacheManager.registerCustomCache(
+//            PERSON_CACHE,
+//            Caffeine.newBuilder().expireAfter(InvaliderCacheFørStartenAvArbeidsdag()).build(),
+//        )
+//        return caffeineCacheManager
+//    }
 }
