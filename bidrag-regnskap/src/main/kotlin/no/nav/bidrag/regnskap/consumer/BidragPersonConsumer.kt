5638ced57e9d430d40b0a8e4937524d132832039
Simen Høston <simen.naper.hoston@webstep.no>
2025-09-10 09:38:57 +0200

    Refaktorerer PersonConsumer: renamer til BidragPersonConsumer, trekker ut URI-bygging til egen metode, og fjerner unødvendig logging

--- bidrag-regnskap/src/main/kotlin/no/nav/bidrag/regnskap/consumer/BidragPersonConsumer.kt
+++ bidrag-regnskap/src/main/kotlin/no/nav/bidrag/regnskap/consumer/BidragPersonConsumer.kt
@@ -1,10 +1,8 @@
 package no.nav.bidrag.regnskap.consumer
 
-import io.github.oshai.kotlinlogging.KotlinLogging
 import no.nav.bidrag.commons.web.client.AbstractRestClient
 import no.nav.bidrag.domene.ident.Ident
 import no.nav.bidrag.domene.ident.Personident
-import no.nav.bidrag.regnskap.SECURE_LOGGER
 import no.nav.bidrag.transport.person.PersonRequest
 import no.nav.bidrag.transport.person.PersondetaljerDto
 import org.springframework.beans.factory.annotation.Qualifier
@@ -14,31 +12,26 @@ import org.springframework.web.client.RestOperations
 import org.springframework.web.util.UriComponentsBuilder
 import java.net.URI
 
-private val LOGGER = KotlinLogging.logger {}
 
 @Service
-class PersonConsumer(
+class BidragPersonConsumer(
     @param:Value("\${PERSON_URL}") val url: URI,
     @param:Qualifier("azure") private val restTemplate: RestOperations,
-) : AbstractRestClient(restTemplate, "bidrag-aktoerregister-aktoerregister") {
+) : AbstractRestClient(restTemplate, "bidrag-aktoerregister") {
 
     companion object {
         private const val PERSON_PATH = "/informasjon/detaljer"
     }
 
     fun hentPerson(personIdent: Ident): PersondetaljerDto? {
-        try {
-            val response: PersondetaljerDto? = postForEntity(
-                UriComponentsBuilder.fromUri(url).path(PERSON_PATH).build().toUri(),
-                PersonRequest(Personident(personIdent.verdi)),
-            )
-            SECURE_LOGGER.debug("Hentet person med id: {} fra bidrag-person: {}", personIdent.verdi, response)
-            return response
-        } catch (e: Exception) {
-            SECURE_LOGGER.error(
-                "Noe gikk galt i kallet mot bidrag-person for ident: ${personIdent.verdi}. Svaret fra bidrag-person var: ${e.message}",
-            )
-            throw e
-        }
+        val personRequestUri = byggPersonRequestUri()
+        val personRequest = PersonRequest(Personident(personIdent.verdi))
+
+        return postForEntity(personRequestUri, personRequest)
     }
+
+    private fun byggPersonRequestUri(): URI = UriComponentsBuilder.fromUri(url)
+        .path(PERSON_PATH)
+        .build()
+        .toUri()
 }
