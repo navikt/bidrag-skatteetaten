c6e8fe432958dc5bd7ba80cd91d88264bf1b0d26
Simen Høston <simen.naper.hoston@webstep.no>
2025-09-16 10:54:19 +0200

    Utvider personhendelseinnlesing til å håndtere både når bidrag-person ikke finner person samt bedre håndtering av manglende id på topic. Endrer fra å bruke securelogger til å bruke vanlig logging

--- bidrag-regnskap/src/main/kotlin/no/nav/bidrag/regnskap/service/HendelseService.kt
+++ bidrag-regnskap/src/main/kotlin/no/nav/bidrag/regnskap/service/HendelseService.kt
@@ -1,13 +1,15 @@
 package no.nav.bidrag.regnskap.service
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import no.nav.bidrag.domene.ident.Ident
 import no.nav.bidrag.domene.ident.Personident
-import no.nav.bidrag.regnskap.SECURE_LOGGER
 import no.nav.bidrag.regnskap.consumer.BidragPersonConsumer
 import no.nav.bidrag.regnskap.persistence.entity.Oppdrag
 import no.nav.bidrag.transport.person.hendelse.Endringsmelding
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+
+private val LOGGER = KotlinLogging.logger {}
 
 @Service
 class HendelseService(
@@ -17,39 +19,47 @@ class HendelseService(
 
     @Transactional
     fun behandlePersonhendelse(endringsmelding: Endringsmelding) {
-        for (endring in endringsmelding.endringer) {
-            if (endring.identendring != null && endring.identendring!!.identifikasjonsnummer != null) {
-                val person = bidragPersonConsumer.hentPerson(Ident(endring.identendring!!.identifikasjonsnummer!!))!!.person.ident
+        endringsmelding.endringer.forEach { endring ->
+            val identifikasjonsnummer = endring.identendring?.identifikasjonsnummer
+            if (identifikasjonsnummer == null) {
+                LOGGER.warn { "Mangler identifikasjonsnummer i endring fra personhendelse. Endring: $endring" }
+                return@forEach
+            }
+            val person = bidragPersonConsumer.hentPerson(Ident(identifikasjonsnummer))?.person?.ident
 
-                endringsmelding.personidenter.forEach {
-                    persistenceService.hentAlleMottakereMedIdent(it)
-                        .filter { mottaker -> mottaker.mottakerIdent != person.verdi }
-                        .forEach { mottaker ->
-                            mottaker.mottakerIdent = person.verdi
-                            logIdentendring(it, person, mottaker)
-                        }
+            if (person == null) {
+                LOGGER.warn { "Person med ident $identifikasjonsnummer ble ikke funnet i bidrag-person" }
+                return@forEach
+            }
 
-                    persistenceService.hentAlleKravhavereMedIdent(it)
-                        .filter { kravhaver -> kravhaver.kravhaverIdent != person.verdi }
-                        .forEach { kravhaver ->
-                            kravhaver.kravhaverIdent = person.verdi
-                            logIdentendring(it, person, kravhaver)
-                        }
+            endringsmelding.personidenter.forEach {
+                persistenceService.hentAlleMottakereMedIdent(it)
+                    .filter { mottaker -> mottaker.mottakerIdent != person.verdi }
+                    .forEach { mottaker ->
+                        mottaker.mottakerIdent = person.verdi
+                        logIdentendring(it, person, mottaker)
+                    }
 
-                    persistenceService.hentAlleSkyldnereMedIdent(it)
-                        .filter { skyldner -> skyldner.skyldnerIdent != person.verdi }
-                        .forEach { skyldner ->
-                            skyldner.skyldnerIdent = person.verdi
-                            logIdentendring(it, person, skyldner)
-                        }
+                persistenceService.hentAlleKravhavereMedIdent(it)
+                    .filter { kravhaver -> kravhaver.kravhaverIdent != person.verdi }
+                    .forEach { kravhaver ->
+                        kravhaver.kravhaverIdent = person.verdi
+                        logIdentendring(it, person, kravhaver)
+                    }
 
-                    persistenceService.hentAlleGjelderMedIdent(it)
-                        .filter { gjelder -> gjelder.gjelderIdent != person.verdi }
-                        .forEach { gjelder ->
-                            gjelder.gjelderIdent = person.verdi
-                            logIdentendring(it, person, gjelder)
-                        }
-                }
+                persistenceService.hentAlleSkyldnereMedIdent(it)
+                    .filter { skyldner -> skyldner.skyldnerIdent != person.verdi }
+                    .forEach { skyldner ->
+                        skyldner.skyldnerIdent = person.verdi
+                        logIdentendring(it, person, skyldner)
+                    }
+
+                persistenceService.hentAlleGjelderMedIdent(it)
+                    .filter { gjelder -> gjelder.gjelderIdent != person.verdi }
+                    .forEach { gjelder ->
+                        gjelder.gjelderIdent = person.verdi
+                        logIdentendring(it, person, gjelder)
+                    }
             }
         }
     }
@@ -59,6 +69,6 @@ class HendelseService(
         nyPersonident: Personident,
         oppdrag: Oppdrag,
     ) {
-        SECURE_LOGGER.info("Personhendelse: $gammelIdent oppdatert til $nyPersonident for oppdragId: ${oppdrag.oppdragId}")
+        LOGGER.debug { "Personhendelse: $gammelIdent oppdatert til $nyPersonident for oppdragId: ${oppdrag.oppdragId}" }
     }
 }
