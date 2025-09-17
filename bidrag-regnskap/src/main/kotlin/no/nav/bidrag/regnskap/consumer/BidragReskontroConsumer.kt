0e00d03ecfdc62b8e431b7790179c670dc5f20ce
Simen Høston <simen.naper.hoston@webstep.no>
2025-09-17 11:44:41 +0200

    Fikser url til å gå mot riktig reskontro endepunkt

--- bidrag-regnskap/src/main/kotlin/no/nav/bidrag/regnskap/consumer/BidragReskontroConsumer.kt
+++ bidrag-regnskap/src/main/kotlin/no/nav/bidrag/regnskap/consumer/BidragReskontroConsumer.kt
@@ -23,7 +23,7 @@ class BidragReskontroConsumer(
     fun hentTransasksjonerForSak(saksnummer: String): TransaksjonerDto? {
         val postForEntity = postForEntity<ResponseEntity<TransaksjonerDto>>(
             bidragReskontroUri
-                .pathSegment("bidragssak")
+                .pathSegment("/transaksjoner/bidragssak")
                 .build()
                 .toUri(),
             SaksnummerRequest(Saksnummer(saksnummer)),
