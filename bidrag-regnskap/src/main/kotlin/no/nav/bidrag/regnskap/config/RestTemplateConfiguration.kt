c6158a953e2d61e8a3426190df23571c8c9916da
Simen Høston <simen.naper.hoston@webstep.no>
2025-10-24 14:09:35 +0200

    Egen interceptor for å sjekke størrelse på request mot Elins krav Api

--- bidrag-regnskap/src/main/kotlin/no/nav/bidrag/regnskap/config/RestTemplateConfiguration.kt
+++ bidrag-regnskap/src/main/kotlin/no/nav/bidrag/regnskap/config/RestTemplateConfiguration.kt
@@ -1,20 +1,29 @@
 package no.nav.bidrag.regnskap.config
 
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import io.github.oshai.kotlinlogging.KotlinLogging
 import no.nav.bidrag.commons.CorrelationId
 import no.nav.bidrag.commons.security.api.EnableSecurityConfiguration
 import no.nav.bidrag.commons.web.HttpHeaderRestTemplate
 import no.nav.bidrag.commons.web.config.RestOperationsAzure
+import no.nav.bidrag.regnskap.consumer.SkattConsumer
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.actuate.metrics.web.client.ObservationRestTemplateCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 import org.springframework.context.annotation.Scope
+import org.springframework.http.HttpRequest
+import org.springframework.http.client.ClientHttpRequestExecution
+import org.springframework.http.client.ClientHttpRequestInterceptor
+import org.springframework.http.client.ClientHttpResponse
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 import org.springframework.http.client.observation.ClientRequestObservationConvention
 import org.springframework.http.client.observation.DefaultClientRequestObservationConvention
 import org.springframework.web.client.RestTemplate
+import java.io.IOException
+
+private val LOGGER = KotlinLogging.logger { }
 
 @Configuration
 @EnableSecurityConfiguration
@@ -35,10 +44,26 @@ class RestTemplateConfiguration {
         restTemplate.withDefaultHeaders()
         restTemplate.addHeaderGenerator("Nav-Callid") { CorrelationId.fetchCorrelationIdForThread() }
         restTemplate.addHeaderGenerator("Nav-Consumer-Id") { naisAppName }
+        restTemplate.interceptors.add(KravApiRequestInterceptor())
         observationRestTemplateCustomizer.customize(restTemplate)
         return restTemplate
     }
 
     @Bean
     fun clientRequestObservationConvention(): ClientRequestObservationConvention = DefaultClientRequestObservationConvention()
+}
+
+class KravApiRequestInterceptor : ClientHttpRequestInterceptor {
+    @Throws(IOException::class)
+    override fun intercept(request: HttpRequest, body: ByteArray, execution: ClientHttpRequestExecution): ClientHttpResponse {
+        if (request.uri.path.contains(SkattConsumer.KRAV_PATH)) {
+            logRequest(request, body)
+        }
+        return execution.execute(request, body)
+    }
+
+    private fun logRequest(request: HttpRequest, body: ByteArray?) {
+        val bodySize = body?.size ?: 0
+        LOGGER.info { "Request URI: ${request.uri}, Method: ${request.method}, Body Size: $bodySize bytes, Header Size: ${request.headers.size}" }
+    }
 }
