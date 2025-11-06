b20464f9d186de4d81af226b8101a11c50de6930
Simen Høston <simen.naper.hoston@webstep.no>
2025-11-06 15:42:32 +0100

    Dropper og så legger til nytt innslag i tabellen i steden for å endre eksisterende kolonne

--- bidrag-regnskap/src/main/resources/db/migration/V6.0.1__siste_feilmelding_hash_liste.sql
+++ bidrag-regnskap/src/main/resources/db/migration/V6.0.1__siste_feilmelding_hash_liste.sql
@@ -1,2 +1,5 @@
 ALTER TABLE oppdrag
-    ALTER COLUMN siste_feilmelding_hash TYPE INT[];
+    DROP COLUMN siste_feilmelding_hash;
+
+ALTER TABLE oppdrag
+    ADD COLUMN siste_feilmelding_hash INT[];
