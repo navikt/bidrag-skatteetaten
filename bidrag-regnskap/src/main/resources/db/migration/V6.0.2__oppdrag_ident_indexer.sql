9b3c4c04e5ef24374489592b4f1caed9bc747812
= <=>
2025-11-18 14:41:41 +0100

    Legger til indekser for mottaker, kravhaver, skyldner og gjelder i oppdrag-tabellen

---
+++ bidrag-regnskap/src/main/resources/db/migration/V6.0.2__oppdrag_ident_indexer.sql
@@ -0,0 +1,4 @@
+CREATE INDEX mottaker_index ON oppdrag (mottaker_ident);
+CREATE INDEX kravhaver_index ON oppdrag (kravhaver_ident);
+CREATE INDEX skyldner_index ON oppdrag (skyldner_ident);
+CREATE INDEX gjelder_index ON oppdrag (gjelder_ident);
