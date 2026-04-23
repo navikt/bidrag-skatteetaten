c2e76f616c46c8822dbbc285138b5c8ef1118023
Simen Høston <simen.naper.hoston@webstep.no>
2026-04-23 14:58:33 +0200

    Endrer ObjectMapper-konfigurasjon i RestConfig

--- bidrag-reskontro/src/main/kotlin/no/nav/bidrag/reskontro/config/RestConfig.kt
+++ bidrag-reskontro/src/main/kotlin/no/nav/bidrag/reskontro/config/RestConfig.kt
@@ -1,8 +1,10 @@
 package no.nav.bidrag.reskontro.config
 
 import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.util.StdDateFormat
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.datatype.jsr310.deser.YearMonthDeserializer
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import no.nav.bidrag.commons.security.api.EnableSecurityConfiguration
 import no.nav.bidrag.commons.web.config.RestOperationsAzure
@@ -12,27 +14,18 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 import org.springframework.http.client.observation.ClientRequestObservationConvention
 import org.springframework.http.client.observation.DefaultClientRequestObservationConvention
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
-import java.time.YearMonth
-import java.time.format.DateTimeFormatter
 
 @Configuration
 @EnableSecurityConfiguration
 @Import(RestOperationsMaskinporten::class, RestOperationsAzure::class)
 class RestConfig {
     @Bean
-    fun jackson2ObjectMapperBuilder(): Jackson2ObjectMapperBuilder = Jackson2ObjectMapperBuilder()
-        .modules(
-            KotlinModule.Builder().build(),
-            JavaTimeModule()
-                .addDeserializer(
-                    YearMonth::class.java,
-                    // Denne trengs for å parse år over 9999 riktig.
-                    YearMonthDeserializer(DateTimeFormatter.ofPattern("u-MM")),
-                ),
-        )
-        .failOnUnknownProperties(false)
-        .serializationInclusion(JsonInclude.Include.NON_NULL)
+    fun objectMapper(): ObjectMapper = ObjectMapper()
+        .registerModule(KotlinModule.Builder().build())
+        .registerModule(JavaTimeModule())
+        .setDateFormat(StdDateFormat())
+        .setDefaultPropertyInclusion(JsonInclude.Include.NON_NULL)
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 
     @Bean
     fun clientRequestObservationConvention(): ClientRequestObservationConvention = DefaultClientRequestObservationConvention()
