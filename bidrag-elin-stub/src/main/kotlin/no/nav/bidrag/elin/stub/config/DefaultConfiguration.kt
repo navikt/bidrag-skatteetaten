1b87166ff90d25f78632592d75e87567f8c146aa
Simen Høston <simen.naper.hoston@webstep.no>
2026-04-23 14:58:52 +0200

    Legger til DisableDefaultSpringSecurityConfiguration i DefaultConfiguration

--- bidrag-elin-stub/src/main/kotlin/no/nav/bidrag/elin/stub/config/DefaultConfiguration.kt
+++ bidrag-elin-stub/src/main/kotlin/no/nav/bidrag/elin/stub/config/DefaultConfiguration.kt
@@ -11,12 +11,15 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
 import io.swagger.v3.oas.annotations.info.Info
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.security.SecurityScheme
+import no.nav.bidrag.commons.security.DisableDefaultSpringSecurityConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.EnableAspectJAutoProxy
+import org.springframework.context.annotation.Import
 
 @EnableAspectJAutoProxy
 @OpenAPIDefinition(info = Info(title = "bidrag-elin-stub", version = "v1"), security = [SecurityRequirement(name = "bearer-key")])
 @SecurityScheme(bearerFormat = "JWT", name = "bearer-key", scheme = "bearer", type = SecuritySchemeType.HTTP)
+@Import(DisableDefaultSpringSecurityConfiguration::class)
 class DefaultConfiguration {
     @Bean
     fun objectMapper(): ObjectMapper = ObjectMapper()
