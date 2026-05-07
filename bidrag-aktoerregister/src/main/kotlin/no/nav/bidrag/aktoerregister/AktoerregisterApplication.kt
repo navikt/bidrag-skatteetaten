4bf77ea1a480f6995068258a88059ffa8bf02cff
Simen Høston <simen.naper.hoston@webstep.no>
2026-05-07 19:33:44 +0200

    Legger til UserDetailsServiceAutoConfiguration i ekskludering av Spring Boot-applikasjonen

--- bidrag-aktoerregister/src/main/kotlin/no/nav/bidrag/aktoerregister/AktoerregisterApplication.kt
+++ bidrag-aktoerregister/src/main/kotlin/no/nav/bidrag/aktoerregister/AktoerregisterApplication.kt
@@ -5,9 +5,10 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration
 import org.springframework.boot.security.autoconfigure.actuate.web.servlet.ManagementWebSecurityAutoConfiguration
 
-@SpringBootApplication(exclude = [SecurityAutoConfiguration::class, ManagementWebSecurityAutoConfiguration::class])
+@SpringBootApplication(exclude = [SecurityAutoConfiguration::class, ManagementWebSecurityAutoConfiguration::class, UserDetailsServiceAutoConfiguration::class])
 @EnableConfigurationProperties
 @EnableJwtTokenValidation(ignore = ["org.springframework", "org.springdoc"])
 class AktoerregisterApplication
