b360a7de776ed064a123dea8f93331dba4ee02c0
Simen Høston <simen.naper.hoston@webstep.no>
2026-05-07 19:30:13 +0200

    Fjerner /r og /n i headere

--- bidrag-elin-stub/src/main/kotlin/no/nav/bidrag/elin/stub/aop/DefaultRestControllerAdvice.kt
+++ bidrag-elin-stub/src/main/kotlin/no/nav/bidrag/elin/stub/aop/DefaultRestControllerAdvice.kt
@@ -6,8 +6,9 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestControllerAdvice
+
+private fun String?.sanitizeHeader(): String = this?.replace("\r", "")?.replace("\n", " ") ?: ""
 
 @RestControllerAdvice
 class DefaultRestControllerAdvice {
@@ -15,21 +16,19 @@ class DefaultRestControllerAdvice {
         private val LOGGER = LoggerFactory.getLogger(DefaultRestControllerAdvice::class.java)
     }
 
-    @ResponseBody
-    @ExceptionHandler(Exception::class)
-    fun handleOtherExceptions(exception: Exception): ResponseEntity<Any> {
-        LOGGER.warn("Det skjedde en ukjent feil", exception)
-        return ResponseEntity
-            .status(HttpStatus.INTERNAL_SERVER_ERROR)
-            .header(HttpHeaders.WARNING, "Det skjedde en ukjent feil: ${exception.message}")
-            .build()
-    }
-
-    @ResponseBody
     @ExceptionHandler(JwtTokenUnauthorizedException::class)
     fun handleUnauthorizedException(exception: JwtTokenUnauthorizedException): ResponseEntity<Any> {
         LOGGER.warn("Ugyldig eller manglende sikkerhetstoken", exception)
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
             .header(HttpHeaders.WARNING, "Ugyldig eller manglende sikkerhetstoken").build()
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handleOtherExceptions(exception: Exception): ResponseEntity<Any> {
+        LOGGER.warn("Det skjedde en ukjent feil", exception)
+        return ResponseEntity
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .header(HttpHeaders.WARNING, "Det skjedde en ukjent feil: ${exception.message.sanitizeHeader()}")
+            .build()
     }
 }
