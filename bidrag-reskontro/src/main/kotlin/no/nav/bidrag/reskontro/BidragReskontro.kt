333c76a4335b88eb0cd09b702502def10c19ce4c
Simen Høston <simen.naper.hoston@webstep.no>
2026-05-08 08:39:14 +0200

    Oppdaterer sikkerhetskonfigurasjon ved å ekskludere SecurityAutoConfiguration og UserDetailsServiceAutoConfiguration

--- bidrag-reskontro/src/main/kotlin/no/nav/bidrag/reskontro/BidragReskontro.kt
+++ bidrag-reskontro/src/main/kotlin/no/nav/bidrag/reskontro/BidragReskontro.kt
@@ -3,13 +3,21 @@ package no.nav.bidrag.reskontro
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration
 import org.springframework.boot.security.autoconfigure.actuate.web.servlet.ManagementWebSecurityAutoConfiguration
 import org.springframework.boot.security.autoconfigure.web.servlet.ServletWebSecurityAutoConfiguration
 
 const val PROFILE_NAIS = "nais"
 
-@SpringBootApplication(exclude = [ServletWebSecurityAutoConfiguration::class, ManagementWebSecurityAutoConfiguration::class])
-@EnableJwtTokenValidation(ignore = ["org.springframework", "org.springdoc"])
+@SpringBootApplication(
+    exclude = [
+        SecurityAutoConfiguration::class,
+        ManagementWebSecurityAutoConfiguration::class,
+        UserDetailsServiceAutoConfiguration::class,
+        ServletWebSecurityAutoConfiguration::class,
+    ]
+)@EnableJwtTokenValidation(ignore = ["org.springframework", "org.springdoc"])
 class BidragReskontro
 
 fun main(args: Array<String>) {
