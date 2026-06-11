a690d83f34b880cb37a368dd7fe0567d69ec1117
Simen Høston <simen.naper.hoston@webstep.no>
2026-06-11 13:36:49 +0200

    Logger feil ved problemer mot reskontro

--- bidrag-regnskap/src/main/kotlin/no/nav/bidrag/regnskap/consumer/BidragReskontroConsumer.kt
+++ bidrag-regnskap/src/main/kotlin/no/nav/bidrag/regnskap/consumer/BidragReskontroConsumer.kt
@@ -1,12 +1,15 @@
 package no.nav.bidrag.regnskap.consumer
 
+import no.nav.bidrag.commons.util.secureLogger
 import no.nav.bidrag.commons.web.client.AbstractRestClient
 import no.nav.bidrag.domene.sak.Saksnummer
 import no.nav.bidrag.transport.reskontro.request.SaksnummerRequest
 import no.nav.bidrag.transport.reskontro.response.transaksjoner.TransaksjonerDto
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.ProblemDetail
 import org.springframework.stereotype.Service
+import org.springframework.web.client.HttpStatusCodeException
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.UriComponentsBuilder
 import java.net.URI
@@ -19,14 +22,17 @@ class BidragReskontroConsumer(
     private val bidragReskontroUri
         get() = UriComponentsBuilder.fromUri(uri)
 
-    fun hentTransasksjonerForSak(saksnummer: String): TransaksjonerDto? {
-        val postForEntity = postForEntity<TransaksjonerDto>(
+    fun hentTransasksjonerForSak(saksnummer: String): TransaksjonerDto? = try {
+        postForEntity<TransaksjonerDto>(
             bidragReskontroUri
                 .pathSegment("transaksjoner/bidragssak")
                 .build()
                 .toUri(),
             SaksnummerRequest(Saksnummer(saksnummer)),
         )
-        return postForEntity
+    } catch (e: HttpStatusCodeException) {
+        val problem = e.getResponseBodyAs(ProblemDetail::class.java)
+        secureLogger.error(e) { "${problem?.title}: ${problem?.status} - ${problem?.detail}" }
+        throw e
     }
 }
