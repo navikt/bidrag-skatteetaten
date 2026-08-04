ebc14b5dc17fe19f95073bb1710ff28ce27eb026
Simen Høston <simen.naper.hoston@webstep.no>
2026-08-04 12:53:27 +0200

    Fikser compile warnings

--- bidrag-aktoerregister/src/main/kotlin/no/nav/bidrag/aktoerregister/dto/AdresseDTO.kt
+++ bidrag-aktoerregister/src/main/kotlin/no/nav/bidrag/aktoerregister/dto/AdresseDTO.kt
@@ -7,28 +7,28 @@ import io.swagger.v3.oas.annotations.media.Schema
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class AdresseDTO(
 
-    @Schema(description = "Aktørens navn", deprecated = true)
+    @param:Schema(description = "Aktørens navn", deprecated = true)
     @Deprecated("Flyttet til NavnDTO.")
     val navn: String? = null,
 
-    @Schema(description = "Første adresselinje inneholder normalt gatenavn, men kan også innehold f.eks c/o.")
+    @param:Schema(description = "Første adresselinje inneholder normalt gatenavn, men kan også innehold f.eks c/o.")
     val adresselinje1: String? = null,
 
-    @Schema(description = "Andre adresselinje brukes primært i utlandsadresser, hvor postnr og poststed ikke er tilgjengelig som strukturerte data.")
+    @param:Schema(description = "Andre adresselinje brukes primært i utlandsadresser, hvor postnr og poststed ikke er tilgjengelig som strukturerte data.")
     val adresselinje2: String? = null,
 
-    @Schema(description = "Tredje adresselinje brukes i noen tilfeller til region.")
+    @param:Schema(description = "Tredje adresselinje brukes i noen tilfeller til region.")
     val adresselinje3: String? = null,
 
-    @Schema(description = "Leilighetsnummer dersom dette er tilgjengelig som strukturerte data.")
+    @param:Schema(description = "Leilighetsnummer dersom dette er tilgjengelig som strukturerte data.")
     val leilighetsnummer: String? = null,
 
-    @Schema(description = "Postnr dersom dette er tilgjengelig som strukturerte data.")
+    @param:Schema(description = "Postnr dersom dette er tilgjengelig som strukturerte data.")
     val postnr: String? = null,
 
-    @Schema(description = "Poststed dersom dette er tilgjengelig som strukturerte data.")
+    @param:Schema(description = "Poststed dersom dette er tilgjengelig som strukturerte data.")
     val poststed: String? = null,
 
-    @Schema(description = "Land som 3-bokstavs land-kode.")
+    @param:Schema(description = "Land som 3-bokstavs land-kode.")
     val land: String? = null,
 )
